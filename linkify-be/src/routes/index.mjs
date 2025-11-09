ea7fa6cd7edf4d38d6b0b6658bb3ccaad2e7cfe5
dorayakiiiiz <travansy2305@gmail.com>
2025-11-10 00:21:42 +0700

    commit

--- linkify-be/src/routes/index.mjs
+++ linkify-be/src/routes/index.mjs
@@ -2,12 +2,13 @@
 import authRouter from "./auth.mjs";
 import homeRouter from "./home.mjs";
 import userRouter from "./user.mjs"
-
+import profileRouter from "./profile.mjs"
 
 export default function route(app) {
 
     app.use('/api/auth', authRouter);
     app.use('/api/user', userRouter);
+    app.use('/api/profile', profileRouter);
 
     app.use('/api', homeRouter);
 }
