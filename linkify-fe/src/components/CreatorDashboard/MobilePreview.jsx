d49012498917b9f974b7b28765dc41699fd20c86
dorayakiiiiz <travansy2305@gmail.com>
2025-11-23 16:20:49 +0700

    bug

--- linkify-fe/src/components/CreatorDashboard/MobilePreview.jsx
+++ linkify-fe/src/components/CreatorDashboard/MobilePreview.jsx
@@ -30,26 +30,26 @@ export default function MobilePreview() {
                 <i className="fa-regular fa-share-from-square"></i>
             </div>
 
-        <div className="relative w-[280px] h-[560px] border-[8px] border-black rounded-[3rem] bg-black shadow-2xl overflow-hidden">
-            
-            {/* tai thỏ */}
-            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-32 h-[18px] bg-black rounded-b-xl z-10"></div>
-
-            {/* content bên trong điện thoại */}
-            <div className="w-full h-full overflow-y-auto no-scrollbar">
-                <LinkTreePreview 
-                    profile={profile}
-                    loading={loading}
-
-                    links={links}
-                    loadingLinks={loadingLinks}
-
-                    isPreview={true}
-                    tab={currentTab}
-                />
+            <div className="relative w-[280px] h-[560px] border-[8px] border-black rounded-[3rem] bg-black shadow-2xl overflow-hidden">
                 
+                {/* tai thỏ */}
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-32 h-[18px] bg-black rounded-b-xl z-10"></div>
+
+                {/* content bên trong điện thoại */}
+                <div className="w-full h-full overflow-y-auto no-scrollbar">
+                    <LinkTreePreview 
+                        profile={profile}
+                        loading={loading}
+
+                        links={links}
+                        loadingLinks={loadingLinks}
+
+                        isPreview={true}
+                        tab={currentTab}
+                    />
+                    
+                </div>
             </div>
-        </div>
         </div>
     );
 }
