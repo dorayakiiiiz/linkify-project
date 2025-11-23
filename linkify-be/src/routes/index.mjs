782c60c339d63c7c74502387efe24c1da5c12039
Otis <ledat02072005@gmail.com>
2025-11-23 12:20:07 +0700

    fix linh tinh

--- linkify-be/src/routes/index.mjs
+++ linkify-be/src/routes/index.mjs
@@ -9,10 +9,6 @@ export default function route(app) {
     app.use('/api/auth', authRouter);
     app.use('/api/user', userRouter);
     app.use('/api/profile', profileRouter);
-<<<<<<< HEAD
-=======
-
->>>>>>> 98d09f1c93f136487e4caa6f3003b152dd181b2b
     app.use('/api', homeRouter);
 
     // link routes (CRUD link)
