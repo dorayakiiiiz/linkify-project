019e6123bcc6042f792925a6c4315b0ba16910fe
HuynhDucThinh <huynhducthinhmath@gmail.com>
2025-11-25 21:17:41 +0700

    Update shop feature files

--- linkify-fe/src/services/shopService.js
+++ linkify-fe/src/services/shopService.js
@@ -1,16 +1,11 @@
-// SHOP FEATURE - Shop service for API calls
 import api from "./api";
 
-// SHOP FEATURE - Get all products by profile ID
 const getProductsByProfile = async (profileId) => {
-  // SHOP FEATURE - Call GET /api/shop/:profileId
   const response = await api.get(`/shop/${profileId}`);
   return response.data;
 };
 
-// SHOP FEATURE - Add new product with image upload
 const addProduct = async (formData) => {
-  // SHOP FEATURE - Send multipart/form-data with product info + image
   const response = await api.post("/shop", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
@@ -19,9 +14,7 @@ const addProduct = async (formData) => {
   return response.data;
 };
 
-// SHOP FEATURE - Update existing product (optional new image)
 const updateProduct = async (itemId, formData) => {
-  // SHOP FEATURE - Call PATCH /api/shop/:itemId with formData
   const response = await api.patch(`/shop/${itemId}`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
@@ -30,14 +23,11 @@ const updateProduct = async (itemId, formData) => {
   return response.data;
 };
 
-// SHOP FEATURE - Delete product by ID
 const deleteProduct = async (itemId) => {
-  // SHOP FEATURE - Call DELETE /api/shop/:itemId
   const response = await api.delete(`/shop/${itemId}`);
   return response.data;
 };
 
-// SHOP FEATURE - Export shop service object
 export const shopService = {
   getProductsByProfile,
   addProduct,
