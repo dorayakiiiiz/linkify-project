483ca9782782b105fb5b0422d5ceb62cdd5d68e1
HuynhDucThinh <huynhducthinhmath@gmail.com>
2025-11-25 21:19:53 +0700

    Update shop feature files

--- linkify-fe/src/pages/PubicProfile.jsx
+++ linkify-fe/src/pages/PubicProfile.jsx
@@ -1,12 +1,12 @@
 import { useProfile } from "../context/ProfileContext";
 import { useLinks } from "../context/LinkContext";
-import { useShop } from "../context/ShopContext"; // SHOP FEATURE - Import Shop Context
+import { useShop } from "../context/ShopContext";
 import LinkTreePreview from "../components/Shared/LinkTreePreview";
 
 export default function PublicProfile() {
   const { profile, loading } = useProfile();
   const { links, loadingLinks } = useLinks();
-  const { products, loadingProducts } = useShop(); // SHOP FEATURE - Get products from Shop Context
+  const { products, loadingProducts } = useShop();
 
   return (
     <div className="w-full flex justify-center items-center bg-[#A6A8AA] md:py-10">
@@ -15,8 +15,8 @@ export default function PublicProfile() {
         links={links}
         loading={loading}
         loadingLinks={loadingLinks}
-        products={products} // SHOP FEATURE - Pass products to preview
-        loadingProducts={loadingProducts} // SHOP FEATURE - Pass loading state
+        products={products}
+        loadingProducts={loadingProducts}
       />
     </div>
   );
